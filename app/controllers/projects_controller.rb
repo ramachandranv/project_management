9a45aacf5db3b51e0aa5fc7eb6e80870ea80cc68
Ramachandran <ramachandran.v@dharanasoft.com>
2012-02-22 12:07:20 +0530

    User can now create, edit and delete projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,44 @@
 class ProjectsController < ApplicationController
   before_filter :authenticate_user!
   def index
+    @projects = current_user.projects.all
+    
+  end
 
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = current_user.projects.build(params[:project])
+    if @project.save
+      redirect_to projects_path(@project)
+    else
+      render :action => 'new'
+    end
+  end
+
+  def edit
+    @project = Project.find(params[:id])
+  end
+
+  def update
+    @project = Project.find(params[:id])
+    if @project.update_attributes(params[:project])
+      redirect_to projects_path(@project)
+    else
+      render :action => 'edit'
+    end
+  end
+
+  def show
+    @project = Project.find(params[:id])
+  end
+
+  def destroy
+    @project = Project.find(params[:id])
+    if @project.destroy
+      redirect_to projects_path
+    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120221133305) do
+ActiveRecord::Schema.define(:version => 20120222054213) do
+
+  create_table "projects", :force => true do |t|
+    t.string   "name"
+    t.text     "description"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
